9d3eab1062f27de153d6095cc33dac1f5356d2bb
Pablo Beloso <pbeloso96@gmail.com>
2020-06-11 21:22:23 -0300

    sape

--- p.py
+++ p.py
@@ -2,26 +2,31 @@ import Crypto
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import binascii
+import bcrypt
 
-privateKey = RSA.generate(3072)
+msg = 'pelado boton'
+hashbd= '$2b$12$l9XyD4K/MIaPFCCJtySQl../BEtEhDg2YfuXftdi8Jfjqr/zDd/3i'
 
-print 'private key:', privateKey
-print ''
-publicKey = privateKey.publickey()
+print (msg)
+print ('')
 
-print 'public key:', publicKey
-print ''
+hashed = bcrypt.hashpw(msg.encode('utf-8'), bcrypt.gensalt(12))
 
-msg = b'maicool puto'
-print msg
-print ''
+print(hashed)
 
-encryptor = PKCS1_OAEP.new(publicKey)
-encrypted = encryptor.encrypt(msg)
-print ''
-print 'encrypted:', encrypted
+print('Comparación con la bd:   ' , bcrypt.checkpw(msg.encode('utf-8'), hashbd.encode('utf-8')))
 
-decryptor = PKCS1_OAEP.new(privateKey)
-decrypted = decryptor.decrypt(encrypted)
-print ''
-print 'Decrypted:', decrypted
+def generar_claves():
+    privateKey = RSA.generate(1024)
+    publicKey = privateKey.publickey()
+
+    return privateKey, publicKey
+
+def encrypt(msg, publicKey):
+    encryptor = PKCS1_OAEP.new(publicKey)
+    encrypted = encryptor.encrypt(msg)
+
+def decrypt(encrypt, privateKey):
+    decryptor = PKCS1_OAEP.new(privateKey)
+    decrypted = decryptor.decrypt(encrypted)
+    
